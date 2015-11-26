63a7cc44c2cfd160474bad69751e1728b0c2654f
Martin Page <wookoouk@gmail.com>
2015-11-26 17:29:33 +0000

    Update make_access_point.sh

--- make_access_point.sh
+++ make_access_point.sh
@@ -42,6 +42,7 @@ echo "setting up hostap"
  echo "auto lo" > /etc/network/interfaces
  echo "iface lo inet loopback" >> /etc/network/interfaces
  echo "iface eth0 inet manual" >> /etc/network/interfaces
+ echo "auto wlan0" >> /etc/network/interfaces
  echo "iface wlan1 inet static" >> /etc/network/interfaces
  echo "address 10.10.0.1" >> /etc/network/interfaces
  echo "netmask 255.255.255.0" >> /etc/network/interfaces
